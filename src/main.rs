38f2fa0dd03bcf51ba99be728660c79b55c9e7b0
Andrew Yan <aky26@cornell.edu>
2020-04-25 22:33:28 -0400

    Implement global config file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use structopt::StructOpt;
 use std::collections::HashMap;
 use std::fs::OpenOptions;
+use std::path::{Path, PathBuf};
 
 const SPITCONFIG: &'static str = ".spitconfig";
 const CREATE: &'static str = "CREATE";
@@ -12,6 +13,14 @@ struct Opt {
     /// Create a new config file.
     #[structopt(long)]
     init: bool,
+
+    /// Copy another config file in FOLDER.
+    #[structopt(long, name = "FOLDER")]
+    copy: Option<PathBuf>,
+
+    /// All actions apply to the global config file.
+    #[structopt(short, long)]
+    global: bool,
 
     /// Assign TEXT to all the supplied names.
     #[structopt(short, long, name="TEXT")]
@@ -26,8 +35,8 @@ struct Opt {
     names: Vec<String>,
 }
 
-fn kill_or(verb: &str, oo: &mut std::fs::OpenOptions) -> std::fs::File {
-    match oo.open(SPITCONFIG) {
+fn kill_or(verb: &str, oo: &std::fs::OpenOptions, config: &Path) -> std::fs::File {
+    match oo.open(config) {
         Ok(v) => v,
         Err(e) => {
             eprintln!("Could not {} '{}': {}", verb, SPITCONFIG, e);
@@ -55,24 +64,80 @@ fn serialize_or_kill(file: std::fs::File, map: &HashMap<String, String>) {
 
 fn main() {
     let opts = Opt::from_args();
-    if opts.init { // initialize new .spitconfig
-        let file = kill_or(CREATE, OpenOptions::new().write(true).create_new(true));
+    let read_options = {
+        let mut temp = OpenOptions::new();
+        temp.read(true);
+        temp
+    };
+    let create_options = {
+        let mut temp = OpenOptions::new();
+        temp.write(true).create_new(true);
+        temp
+    };
+
+    let config: PathBuf = 
+    {
+        let mut temp = 
+        if opts.global { // be lazy and don't alert them that home doesn't exist TODO
+            dirs::home_dir().unwrap_or(PathBuf::new())
+        }
+        else { PathBuf::new() };
+        temp.push(SPITCONFIG);
+        temp
+    };
+    /* if global, then 
+        config = global config
+        global = empty hashmap
+        else,
+        config = local config
+        global = global config
+    */
+
+
+
+
+    if let Some(mut path) = opts.copy {
+        path.push(SPITCONFIG);
+        let mut origin = kill_or("find", &read_options, &path);
+        let mut target = kill_or(CREATE, &create_options, &config);
+        if let Err(e) = std::io::copy(&mut origin, &mut target) {
+            eprintln!("Could not write to '{}': {}", SPITCONFIG, e);
+            std::process::exit(1)
+        };
+    }
+    else if opts.init { // initialize new .spitconfig
+        let file = kill_or(CREATE, &create_options, &config);
         serialize_or_kill(file, &HashMap::<String, String>::new());
     }
-    else if let Some(text) = opts.add { // add to .spitconfig
-        let read_file = kill_or(OPEN, OpenOptions::new().read(true));
+
+    if let Some(text) = opts.add { // add to .spitconfig
+        let read_file = kill_or(OPEN, &read_options, &config);
         let mut spit: HashMap<String, String> = deserialize_or_kill(read_file);
         for name in opts.names {
             spit.insert(name, text.clone()); // maybe add verbose here?
         }
-        let write_file = kill_or(OPEN, OpenOptions::new().write(true));
+        let write_file = kill_or(OPEN, &OpenOptions::new().write(true), &config);
         serialize_or_kill(write_file, &spit);
     }
-    else {
-        let file = kill_or(OPEN, OpenOptions::new().read(true));
+    else { // no options
+        let file = kill_or(OPEN, &read_options, &config);
         let spit: HashMap<String, String> = deserialize_or_kill(file);
+        let global: HashMap<String, String> = 
+        if opts.global { HashMap::new() }
+        else {
+            dirs::home_dir()
+            .map_or(HashMap::new(),
+            |mut path| {
+                path.push(SPITCONFIG);    
+                read_options.open(path)
+                .map_or(HashMap::new(),
+                |file| { 
+                    serde_json::from_reader(file).unwrap_or(HashMap::new()) 
+                })
+            })
+        };
         for name in opts.names {
-            match spit.get(name.as_str()) {
+            match spit.get(name.as_str()).or(global.get(name.as_str())) {
                 None => eprintln!("Couldn't find {}", name),
                 Some(v) => print!("{}{}", v, opts.sep)
             }
