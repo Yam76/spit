49539a47d1f293c0742431d2edb2ecea184d7345
Andrew Yan <aky26@cornell.edu>
2020-04-26 13:53:43 -0400

    Implement warning and pass-through

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,21 @@ use std::fs::OpenOptions;
 use std::path::{Path, PathBuf};
 
 const SPITCONFIG: &'static str = ".spitconfig";
-const CREATE: &'static str = "CREATE";
-const OPEN: &'static str = "OPEN";
+const CREATE: &'static str = "create";
+const OPEN: &'static str = "open";
 
 #[derive(StructOpt)]
 /// Abbreviate frequently typed phrases.
 struct Opt {
     /// Create a new config file.
-    #[structopt(long)]
+    #[structopt(long, conflicts_with("copy"))]
     init: bool,
 
     /// Copy another config file in FOLDER.
-    #[structopt(long, name = "FOLDER")]
+    #[structopt(long, name = "FOLDER", conflicts_with("init"))]
     copy: Option<PathBuf>,
 
-    /// All actions apply to the global config file.
+    /// All actions use the global instead of the local config file.
     #[structopt(short, long)]
     global: bool,
 
@@ -33,6 +33,14 @@ struct Opt {
     /// List of names.
     #[structopt(name = "NAME")]
     names: Vec<String>,
+
+    /// Warn about invalid names instead of exiting. 
+    #[structopt(short, long)]
+    warn: bool,
+
+    /// Pass invalid names through instead of exiting. 
+    #[structopt(short, long)]
+    pass: bool,
 }
 
 fn kill_or(verb: &str, oo: &std::fs::OpenOptions, config: &Path) -> std::fs::File {
@@ -75,28 +83,24 @@ fn main() {
         temp
     };
 
-    let config: PathBuf = 
-    {
+    // if global then config = global config, else local config
+    let config: PathBuf = {
         let mut temp = 
-        if opts.global { // be lazy and don't alert them that home doesn't exist TODO
-            dirs::home_dir().unwrap_or(PathBuf::new())
+        if opts.global { 
+            match dirs::home_dir() {
+                Some(v) => v,
+                None => {
+                    eprintln!("Couldn't find home directory.");
+                    std::process::exit(1)
+                }
+            }
         }
         else { PathBuf::new() };
         temp.push(SPITCONFIG);
         temp
     };
-    /* if global, then 
-        config = global config
-        global = empty hashmap
-        else,
-        config = local config
-        global = global config
-    */
 
-
-
-
-    if let Some(mut path) = opts.copy {
+    if let Some(mut path) = opts.copy.clone() {
         path.push(SPITCONFIG);
         let mut origin = kill_or("find", &read_options, &path);
         let mut target = kill_or(CREATE, &create_options, &config);
@@ -122,6 +126,7 @@ fn main() {
     else { // no options
         let file = kill_or(OPEN, &read_options, &config);
         let spit: HashMap<String, String> = deserialize_or_kill(file);
+        // if global, then global = empty hashmap else, global = global config 
         let global: HashMap<String, String> = 
         if opts.global { HashMap::new() }
         else {
@@ -130,17 +135,37 @@ fn main() {
             |mut path| {
                 path.push(SPITCONFIG);    
                 read_options.open(path)
-                .map_or(HashMap::new(),
+                .map_or(HashMap::new(), 
                 |file| { 
                     serde_json::from_reader(file).unwrap_or(HashMap::new()) 
                 })
             })
-        };
+        };        
+
+        let mut output = String::new();
         for name in opts.names {
             match spit.get(name.as_str()).or(global.get(name.as_str())) {
-                None => eprintln!("Couldn't find {}", name),
-                Some(v) => print!("{}{}", v, opts.sep)
+                Some(text) => {
+                    output.push_str(&text);
+                    output.push_str(&opts.sep);
+                },
+                None => 
+                if opts.pass { 
+                    output.push_str(&name);
+                    output.push_str(&opts.sep);
+                    if opts.warn { // pass and warn = out and warn
+                        eprintln!("Couldn't find {}", name)
+                    }
+                    // pass and no warn = only out
+                }
+                else { 
+                    eprintln!("Couldn't find {}", name); // no pass and warn = only warn
+                    if !opts.warn { // no pass and no warn = quit
+                        std::process::exit(1)
+                    }
+                }
             }
         }
+        print!("{}", output);
     }
 }
